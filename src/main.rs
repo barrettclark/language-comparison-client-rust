3b2f7bbaf805a020d9c8abc18ef62265490ee731
Barrett Clark <barrett@websitesbybarrett.com>
2015-07-14 14:09:32 -0500

    Make GET request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate hyper;
+
+use std::io::Read;
+use hyper::Client;
+use hyper::header::Connection;
+
 fn main() {
-    println!("Hello, world!");
+    // Create a client.
+    let client = Client::new();
+
+    // Creating an outgoing request.
+    let mut res = client.get("http://localhost:9292/")
+        // set a header
+        .header(Connection::close())
+        // let 'er go!
+        .send().unwrap();
+
+    // Read the Response.
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
